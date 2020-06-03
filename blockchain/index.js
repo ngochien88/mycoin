6534e5ff760ee7d09deeabe86928e9b45431f5cf
ngochien88 <57312147+ngochien88@users.noreply.github.com>
2020-06-03 11:00:00 +0100

    bosungblockchain

---
+++ blockchain/index.js
@@ -0,0 +1,61 @@
+const Block = require("./block");
+
+class Blockchain {
+  constructor() {
+    this.chain = [Block.genesis()];
+  }
+
+  addBlock(data) {
+    const block = Block.mineBlock(this.chain[this.chain.length - 1], data);
+    this.chain.push(block);
+    return block;
+  }
+
+  isValidBlock(block) {
+    const lastBlock = this.chain[this.chain.length - 1];
+    if (block.hash !== Block.blockHash(block)) return false;
+    if (block.lastHash != lastBlock.hash) return false;
+    return true;
+  }
+
+  isValidChain(chain) {
+    if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+      console.log("Blockchain -> isValidChain -> chain[0]", chain[0]);
+      console.log(
+        "Blockchain -> isValidChain -> Block.genesis()",
+        Block.genesis()
+      );
+      console.log("fail genesis");
+      // console.log(JSON.stringify(chain[0]));
+      // console.log(JSON.stringify(Block.genesis()));
+      return false;
+    }
+
+    for (let i = 1; i < chain.length; i++) {
+      const block = chain[i];
+      const lastBlock = chain[i - 1];
+      if (
+        block.lastHash !== lastBlock.hash ||
+        block.hash !== Block.blockHash(block)
+      )
+        return false;
+    }
+
+    return true;
+  }
+
+  replaceChain(newChain) {
+    if (newChain.length <= this.chain.length) {
+      console.log("Recieved chain is not longer than the current chain");
+      return;
+    } else if (!this.isValidChain(newChain)) {
+      console.log("Recieved chain is invalid");
+      return;
+    }
+
+    console.log("Replacing the current chain with new chain");
+    this.chain = newChain;
+  }
+}
+
+module.exports = Blockchain;
