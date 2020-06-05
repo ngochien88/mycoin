3b2ffd15cb5de12f0306ce7f4f91853cf27cb537
ngochien88 <57312147+ngochien88@users.noreply.github.com>
2020-06-05 19:00:00 +0100

    suablock2

--- blockchain/index.test.js
+++ blockchain/index.test.js
@@ -8,7 +8,32 @@ describe("Blockchain", () => {
     blockchain2 = new Blockchain();
   });
 
- 
+  it("starts with the genesis block", () => {
+    expect(blockchain.chain[0]).toEqual(Block.genesis());
+  });
+
+  it("adds a new block", () => {
+    const data = "foo";
+    blockchain.addBlock(data);
+    expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(data);
+  });
+
+  it("validates a valid chain", () => {
+    blockchain2.addBlock("foo");
+    console.log(blockchain2.chain);
+    // conventional method for check true and false is toBe
+    expect(blockchain.isValidChain(blockchain2.chain)).toBe(true);
+  });
+
+  it("invalidates a chain with a corrupt the genesis block", () => {
+    blockchain2.chain[0].data = "bad data";
+
+    expect(blockchain.isValidChain(blockchain2.chain)).toBe(false);
+  });
+
+  it("invalidates a corrput chain", () => {
+    blockchain2.addBlock("foo");
+    blockchain2.chain[1].data = "not foo";
 
     expect(blockchain.isValidChain(blockchain2.chain)).toBe(false);
   });
