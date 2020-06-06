f3c796f2a4d0b661ab567acd8f0ea12f03f99ce4
ngochien88 <57312147+ngochien88@users.noreply.github.com>
2020-06-06 19:00:00 +0100

    suabappvaview

--- app.js
+++ app.js
@@ -17,10 +17,9 @@ app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(logger("dev"));
 app.use(express.json());
-
-app.use(bodyParser.json());
-app.use(logger("dev"));
-app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
@@ -36,6 +35,9 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 module.exports = app;
